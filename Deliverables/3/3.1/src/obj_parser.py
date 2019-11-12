8dd881c1f8f69b89fa33e9ea377878fb9e55c67e
petersenea <Petersenea@sbcglobal.net>
2019-11-12 16:36:36 -0600

    stuff

--- Deliverables/3/3.1/src/obj_parser.py
+++ Deliverables/3/3.1/src/obj_parser.py
@@ -1,8 +1,11 @@
 from board import Board
 from stone import Stone
 from constants import BOARD_DIM
+from exceptions import GoCrazyException
 
 def parse_board(matr):
+   if (len(matr)!= BOARD_DIM or len(matr[0]) != BOARD_DIM):
+      raise GoCrazyException
    return Board([[Stone(matr[i][j]) for i in range(BOARD_DIM)] for j in range(BOARD_DIM)])
 
 def parse_boards(arr):
