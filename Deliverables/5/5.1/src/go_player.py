b65dae3144757db39e022af249f87f2f7fe4ff8d
petersenea <Petersenea@sbcglobal.net>
2019-11-12 14:32:17 -0600

    Update go_player.py

--- Deliverables/5/5.1/src/go_player.py
+++ Deliverables/5/5.1/src/go_player.py
@@ -39,44 +39,44 @@ class GoPlayerContract(GoPlayer):
       return self.player.name
    
    def register(self, name="no name"):
-      if self.is_registered:
-         raise Exception("register called multiple times")
-      else:
-         self.is_registered = True
-      return self.player.register(name)
-
-
-      # if self.player.name != None:
+      # if self.is_registered:
       #    raise Exception("register called multiple times")
+      # else:
+      #    self.is_registered = True
       # return self.player.register(name)
+
+
+      if self.player.name != None:
+         raise Exception("register called multiple times")
+      return self.player.register(name)
    
    def receive_stone(self, stone_type):
-      if not self.is_registered:
-         raise Exception("receive called before register")
-      elif self.has_stones:
-         raise Exception("receive called twice")
-      else:
-         self.has_stones = True
-      self.player.receive_stones(stone_type)
-
-
-      # if not self.player.name:
+      # if not self.is_registered:
       #    raise Exception("receive called before register")
-      # elif self.player.stone_type:
+      # elif self.has_stones:
       #    raise Exception("receive called twice")
-      # self.player.receive_stone(stone_type)
+      # else:
+      #    self.has_stones = True
+      # self.player.receive_stones(stone_type)
+
+
+      if not self.player.name:
+         raise Exception("receive called before register")
+      elif self.player.stone_type:
+         raise Exception("receive called twice")
+      self.player.receive_stone(stone_type)
    
    def choose_move(self, boards):
-      if not self.has_stones:
-         raise Exception("choose move called before receive")
-      if len(boards) > 3:
-         raise Exception("bad boards passed in")
-      return self.player.choose_move(boards)
-
-      # if not self.player.stone_type:
+      # if not self.has_stones:
       #    raise Exception("choose move called before receive")
       # if len(boards) > 3:
       #    raise Exception("bad boards passed in")
       # return self.player.choose_move(boards)
+
+      if not self.player.stone_type:
+         raise Exception("choose move called before receive")
+      if len(boards) > 3:
+         raise Exception("bad boards passed in")
+      return self.player.choose_move(boards)
 
 
